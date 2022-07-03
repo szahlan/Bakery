231d6f9e9229c9bfb670fd882dcfab17ca6dc5ff
Sana Zahlan <sanazahlan@yahoo.com>
2022-07-03 12:02:55 -0700

    Create a constructor for pastry class with testing.

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -6,6 +6,11 @@ namespace Bakery.Tests
   [TestClass]
   public class PastryTests
   {
-
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
   }
 }

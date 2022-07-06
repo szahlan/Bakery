8b5a4e8e6efe9efdc03fffceaf333e783606cdd0
Sana Zahlan <sanazahlan@yahoo.com>
2022-07-05 18:34:51 -0700

    add individual pastry price to pastry class with passing test.

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -12,5 +12,13 @@ namespace Bakery.Tests
       Pastry newPastry = new Pastry();
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+
+    [TestMethod]
+    public void GetPastryIndividualPrice_ReturnPastryIndividualPrice_Int()
+    {
+      Pastry newPastry = new Pastry();
+      int result = newPastry.IndividualPrice;
+      Assert.AreEqual(2, result);
+    }
   }
 }

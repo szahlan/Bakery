063e3a313587510710a815e23a2a52692fa8702c
Sana Zahlan <sanazahlan@yahoo.com>
2022-07-05 18:56:38 -0700

    Get quantity of bread to be purchased with passing test.

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -9,16 +9,25 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(4);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
 
     [TestMethod]
     public void GetBreadIndividualPrice_ReturnBreadIndividualPrice_Int()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(4);
       int result = newBread.IndividualPrice;
       Assert.AreEqual(5, result);
+    }
+
+    [TestMethod]
+    public void GetBreadQuantity_ReturnBreadQuantity_Int()
+    {
+      int quantityToPurches = 4;
+      Bread newBread = new Bread(quantityToPurches);
+      int result = newBread.Quantity;
+      Assert.AreEqual(4, result);
     }
   }
 }

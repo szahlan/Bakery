26dbdbc458fa8deebb9bc845372f98cf1cce2296
Sana Zahlan <sanazahlan@yahoo.com>
2022-07-05 18:59:01 -0700

    Get quantity of pastry to purchase with passing test.

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -9,16 +9,25 @@ namespace Bakery.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(4);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
 
     [TestMethod]
     public void GetPastryIndividualPrice_ReturnPastryIndividualPrice_Int()
     {
-      Pastry newPastry = new Pastry();
+      Pastry newPastry = new Pastry(4);
       int result = newPastry.IndividualPrice;
       Assert.AreEqual(2, result);
+    }
+
+    [TestMethod]
+    public void GetBreadQuantity_ReturnBreadQuantity_Int()
+    {
+      int quantityToPurches = 4;
+      Pastry newPastry = new Pastry(quantityToPurches);
+      int result = newPastry.Quantity;
+      Assert.AreEqual(4, result);
     }
   }
 }
